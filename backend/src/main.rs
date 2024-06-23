ce0ec4791594bda76c4445faef758d676043e8a3
Simon Twogood <simon.twogood@gmail.com>
2024-06-23 08:39:20 -0600

    Basic hello world example

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use tide::Request;
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    let mut app = tide::new();
+    app.at("/message").get(say_hello);
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
+
+async fn say_hello(req: Request<()>) -> tide::Result {
+    let message = "Hello, world!";
+    Ok(format!("{}", message).into())
 }
--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -1,4 +1,13 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+
+
+export const loader = async () => {
+    const backend_message = await fetch("http://localhost:8080/message").then(res => res.text());
+
+    return json({ backend_message});
+}
+
 
 export const meta: MetaFunction = () => {
   return [
@@ -8,41 +17,14 @@ export const meta: MetaFunction = () => {
 };
 
 export default function Index() {
+  const { backend_message } = useLoaderData<typeof loader>();
+
   return (
-    <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
+      <div>
+        <h1 className="text-3xl">Full stack</h1>
+        <p className="text-lg">
+          { backend_message }
+        </p>
+      </div>
   );
 }
